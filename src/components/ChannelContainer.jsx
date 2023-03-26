f71903eba0933b2244ae9061be31b566b850aa02
Saif Ali <saifaly.jariya@gmail.com>
2023-03-26 13:00:42 +0500

    Channel and Chats: added channel and chat view

--- src/components/ChannelContainer.jsx
+++ src/components/ChannelContainer.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Channel, useChatContext, MessageSimple } from 'stream-chat-react';
 
-function ChannelContainer() {
+import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from './';
+
+function ChannelContainer({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) {
+  const { channel } = useChatContext();
+
+  if(isCreating) {
+    return (
+      <div className='channel__container'>
+        <CreateChannel createType={createType} setIsCreating={setIsCreating} />
+      </div>
+    )
+  }
+
+  if(isEditing) {
+    return (
+      <div className='channel__container'>
+        <EditChannel setIsEditing={setIsEditing} />
+      </div>
+    )
+  }
+
+  const EmptyState = () => (
+    <div className='channel-empty__container'>
+      <p className='channel-empty__first'>This is the beginning of your chat history</p>
+      <p className='channel-empty__second'>Send messages, attachments, links, emojis, and more!</p>
+    </div>
+  )
+
   return (
-    <div>ChannelContainer</div>
+    <div className='channel__container'>
+      <Channel
+        EmptyStateIndicator={EmptyState}
+        Message={(messageProps, i) => <MessageSimple key={i} {...messageProps} />}
+        setIsEditing={setIsEditing}
+      >
+        <ChannelInner />
+      </Channel>
+    </div>
   )
 }
 
