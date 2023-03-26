2a479ef123602af74942cf71d42aeee6851157f9
Saif Ali <saifaly.jariya@gmail.com>
2023-03-26 14:39:11 +0500

    Bug: channel list not appearing on sidebar

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ChannelList, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
@@ -31,7 +31,17 @@ const CompanyHeader = () => (
   </div>
 )
 
-function ChannelListContainer({ isCreating, setIsCreating, setCreateType, setIsEditing }) {
+const customChannelTeamFilter = (channels) => {
+  return channels.filter((channel) => channel.type === 'team');
+}
+
+const customChannelMessagingFilter = (channels) => {
+  return channels.filter((channel) => channel.type === 'messaging');
+}
+
+const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEditing }) => {
+  const { client } = useChatContext();
+
   const logout = () => {
     cookies.remove("token");
     cookies.remove('userId');
@@ -44,6 +54,8 @@ function ChannelListContainer({ isCreating, setIsCreating, setCreateType, setIsE
     window.location.reload();
   }
 
+  const filters = { members: { $in: [client.userID] } };
+
   return (
     <>
       <SideBar logout={logout} />
@@ -51,8 +63,8 @@ function ChannelListContainer({ isCreating, setIsCreating, setCreateType, setIsE
         <CompanyHeader />
         <ChannelSearch />
         <ChannelList 
-          filters={{}}
-          channelRenderFilterFn={() => {}}
+          filters={filters}
+          channelRenderFilterFn={customChannelTeamFilter}
           List={(listProps) => (
             <TeamChannelList 
               {...listProps} 
@@ -71,8 +83,8 @@ function ChannelListContainer({ isCreating, setIsCreating, setCreateType, setIsE
           )}
         />
         <ChannelList 
-          filters={{}}
-          channelRenderFilterFn={() => {}}
+          filters={filters}
+          channelRenderFilterFn={customChannelMessagingFilter}
           List={(listProps) => (
             <TeamChannelList 
               {...listProps} 
@@ -89,6 +101,34 @@ function ChannelListContainer({ isCreating, setIsCreating, setCreateType, setIsE
               type="messaging" 
             />
           )}
+        />
+      </div>
+    </>
+  )
+}
+
+const ChannelListContainer = ({ setCreateType, setIsCreating, setIsEditing }) => {
+  const [toggleContainer, setToggleContainer] = useState(false);
+
+  return (
+    <>
+      <div className="channel-list__container">
+        <ChannelListContent 
+          setIsCreating={setIsCreating} 
+          setCreateType={setCreateType} 
+          setIsEditing={setIsEditing} 
+        />
+      </div>
+
+      <div className="channel-list__container-responsive"
+        style={{ left: toggleContainer ? "0%" : "-89%", backgroundColor: "#005fff"}}
+      >
+        <div className="channel-list__container-toggle" onClick={() => setToggleContainer((prevToggleContainer) => !prevToggleContainer)}></div>
+        <ChannelListContent 
+          setIsCreating={setIsCreating} 
+          setCreateType={setCreateType} 
+          setIsEditing={setIsEditing}
+          setToggleContainer={setToggleContainer}
         />
       </div>
     </>
--- src/components/TeamChannelList.jsx
+++ src/components/TeamChannelList.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 import { AddChannel } from '../assets';
 
-function TeamChannelList({ childern, error = false, loading, type, isCreating, setIsCreating, setCreateType, setIsEditing }) {
+const TeamChannelList = ({ setToggleContainer, children, error = false, loading, type, isCreating, setIsCreating, setCreateType, setIsEditing }) => {
   if(error) {
     return type === 'team' ? (
-      <div className='team-channel-list'>
-        <p className='team-channel-list__message'>
-          Connection error, please wait a moment and try again.
-        </p>
+      <div className="team-channel-list">
+          <p className="team-channel-list__message">
+              Connection error, please wait a moment and try again.
+          </p>
       </div>
     ) : null
   }
 
   if(loading) {
     return (
-      <div className='team-channel-list'>
-        <p className='team-channel-list__message loading'>
+      <div className="team-channel-list">
+        <p className="team-channel-list__message loading">
           {type === 'team' ? 'Channels' : 'Messages'} loading...
         </p>
       </div>
@@ -24,24 +24,23 @@ function TeamChannelList({ childern, error = false, loading, type, isCreating, s
   }
 
   return (
-    <div className='team-channel-list'>
-      <div className='team-channel-list__header'>
-        <p className='team-channel-list__header__title'>
+    <div className="team-channel-list">
+      <div className="team-channel-list__header">
+        <p className="team-channel-list__header__title">
           {type === 'team' ? 'Channels' : 'Direct Messages'}
         </p>
-        {/* Button - add channel*/}
-        <AddChannel
-          isCreating={isCreating} 
-          setIsCreating={setIsCreating} 
+        <AddChannel 
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
           setCreateType={setCreateType} 
           setIsEditing={setIsEditing}
           type={type === 'team' ? 'team' : 'messaging'}
+          setToggleContainer={setToggleContainer}
         />
       </div>
-      {childern}
+      {children}
     </div>
   )
-
 }
 
 export default TeamChannelList
